9e135cee068ac4ef1b35701beb95f2f593590fd8
GordanMasic <gordan.masic@BitCamps-Mac-mini.local>
2015-06-11 17:24:02 +0200

    new

--- Arrays2Dexercises/src/ba/bitcamp/arrays/Task3_StudentGrades.java
+++ Arrays2Dexercises/src/ba/bitcamp/arrays/Task3_StudentGrades.java
@@ -42,19 +42,13 @@ public class Task3_StudentGrades {
 	public static String[] gradesOfOneStudent(){
 		TextIO.readFile("src/ba/bitcamp/gordan/grades.in");
 		String gradesOneStudents = TextIO.getln();
-		int counter = 0;
+		String trimGrade = gradesOneStudents.replace(" ", "");
 		
-		for (int i = 0; i < gradesOneStudents.length(); i++) {
-			if(gradesOneStudents.charAt(i)!= ' '){
-				counter++;
-			}
-		}
+		String[] array = new String[trimGrade.length()];
 		
-		String[] array = new String[counter];
-		int j = 0;
 		for (int i = 0; i < array.length; i++) {
-			array[i] = ""+gradesOneStudents.charAt(j);
-			j+=2;
+			array[i] = ""+trimGrade.charAt(i);
+			
 		}
 		TextIO.readStandardInput();
 		return array;
@@ -121,7 +115,63 @@ public class Task3_StudentGrades {
 	
 	
 	
-	
+	/**
+	 * This method makes files.
+	 * @param names = string array with file names
+	 */
+	public static void studentFile(String[] names) {
+
+		for (int i = 0; i < names.length; i++) {
+			String name = names[i] + ".grd";
+			TextIO.writeFile(name);
+		}
+	}
+
+	/**
+	 * This method reads names of students from file, discard commas, blank
+	 * spaces, and then turns it to lower case letters.
+	 * 
+	 * @return
+	 * 
+	 *         Array of strings, file names.
+	 */
+	public static String[] makeFileName() {
+
+		TextIO.readFile("src/ba/bitcamp/arrays/names.in");
+
+		int counter = 0;
+
+		while (!TextIO.eof()) {
+			TextIO.getln();
+			counter++;
+		}
+
+		TextIO.readStandardInput();
+
+		TextIO.readFile("src/ba/bitcamp/arrays/names.in");
+
+		String[] studentFile = new String[counter];
+
+		for (int i = 0; i < counter; i++) {
+			String line = TextIO.getln();
+			int length = line.length();
+
+			String rest = "";
+			String name = "";
+			if (line.charAt(length - 3) != ' ') {
+				name = line.substring(0, length - 4);
+				rest = line.substring(length - 3, length - 1);
+			} else {
+				name = line.substring(0, length - 3);
+				rest = line.substring(length - 2, length - 1);
+			}
+			String filename = name + rest;
+
+			studentFile[i] = filename.toLowerCase();
+		}
+
+		return studentFile;
+	}
 	
 	
 	
