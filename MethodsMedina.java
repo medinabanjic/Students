2685dd44ff5ab0a5dca5381de8455db6086b9610
Medina Banjic <medina.banjic@BitCamps-Mac-mini-3.local>
2015-06-11 16:03:09 +0200

    make file names

---
+++ MethodsMedina.java
@@ -0,0 +1,50 @@
+package ba.bitcamp.arrays;
+
+public class MethodsMedina {
+
+	/**
+	 * This method reads names of students from file, discard commas, blank
+	 * spaces, and then turns it to lower case letters.
+	 * 
+	 * @return
+	 * 
+	 *         Array of strings, file names.
+	 */
+	public static String[] makeFileName() {
+
+		TextIO.readFile("src/ba/bitcamp/arrays/names.in");
+
+		int counter = 0;
+
+		while (!TextIO.eof()) {
+			TextIO.getln();
+			counter++;
+		}
+
+		TextIO.readStandardInput();
+
+		TextIO.readFile("src/ba/bitcamp/arrays/names.in");
+
+		String[] studentFile = new String[counter];
+
+		for (int i = 0; i < counter; i++) {
+			String line = TextIO.getln();
+			int length = line.length();
+
+			String rest = "";
+			String name = "";
+			if (line.charAt(length - 3) != ' ') {
+				name = line.substring(0, length - 4);
+				rest = line.substring(length - 3, length - 1);
+			} else {
+				name = line.substring(0, length - 3);
+				rest = line.substring(length - 2, length - 1);
+			}
+			String filename = name + rest;
+
+			studentFile[i] = filename.toLowerCase();
+		}
+
+		return studentFile;
+	}
+}
